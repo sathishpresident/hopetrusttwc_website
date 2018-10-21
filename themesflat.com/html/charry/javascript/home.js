a3a77c351a8a72317709f9b245ba4d6c41791704
Jagadeesh Y M <jagadeesh1113@gmail.com>
2018-10-21 11:05:41 +0530

    changes in home and contact

--- themesflat.com/html/charry/javascript/home.js
+++ themesflat.com/html/charry/javascript/home.js
@@ -148,10 +148,90 @@
         divToPlace.innerHTML = html;
     }
 
+    var fundsAllocated = function () {
+        var trustAmount = 100000;
+        var fundsData = [
+            {
+                imageUrl : 'images/about/index-1/3.jpg',
+                title : 'Education',
+                spendAmount : 20000
+            },
+            {
+                imageUrl : 'images/about/index-1/5.jpg',
+                title : 'Society',
+                spendAmount : 40000
+            },
+            {
+                imageUrl : 'images/about/index-1/4.jpg',
+                title : 'Awareness',
+                spendAmount : 25000
+            },
+            {
+                imageUrl : 'images/about/index-1/6.jpg',
+                title : 'Old Age Homes',
+                spendAmount : 15000
+            }
+        ];
+
+        var divToPlace = document.getElementById('recent-problem-causes');
+
+        var html = fundsData.map((fund, index) => {
+            const percentageUse = (fund.spendAmount / trustAmount) * 100;
+            return `<div class="post">
+            <div class="wrap-post">
+                <div class="img-recent-causes">
+                    <a href="#">
+                        <img src=${fund.imageUrl} alt="image">
+                    </a>
+                </div>
+                <div class="post-recent-causes">
+                    <h6 class="title-post">
+                        <a href="#">${fund.title}</a>
+                    </h6>
+                </div>
+                <div class="donation-amount">
+                    <div class="donation-v1">
+                        <p>${fund.spendAmount}</p>
+                        <span>/</span>
+                    </div>
+                    <div class="donation-v2">
+                        <p>${trustAmount}</p>
+                    </div>
+                </div>
+                <div class="flat-progress">
+                    <div class="perc">${percentageUse}%</div>
+                    <div class="progress-bar" data-percent=${percentageUse} data-waypoint-active="yes">
+                        <div class="progress-animate"></div>
+                    </div>
+                </div>
+            </div>
+        </div>`
+        }).join('');
+        divToPlace.innerHTML = html;
+        progressBar();
+    }
+
+    var progressBar = function () {
+        $('.progress-bar').on('on-appear', function () {
+            $(this).each(function () {
+                var percent = $(this).data('percent');
+
+                $(this).find('.progress-animate').animate({
+                    "width": percent + '%'
+                }, 3000);
+
+                $(this).parent('.flat-progress').find('.perc').addClass('show').animate({
+                    "width": percent + '%'
+                }, 3000);
+            });
+        });
+    };
+
     // Dom Ready
     $(function () {
         bestVolunteers();
         allCount();
+        fundsAllocated();
     });
 
 })(jQuery);
